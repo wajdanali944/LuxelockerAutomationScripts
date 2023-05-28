2621ac5ab120f1673c47ed77455e851e4919ed43
Usama <wajdanali933@gmail.com>
2023-05-28 21:21:13 +0500

    Added configurations and utilities folder with ReadConfigurations files and config.ini

---
+++ test/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+from utilities import ReadConfigurations
+
+
+@pytest.fixture()
+def setup_and_teardown(request):
+    driver = None
+    browser = ReadConfigurations.read_configuration("basic info","browser")
+    if browser.__eq__("chrome"):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser.__eq__("firefox"):
+        driver = webdriver.Firefox(GeckoDriverManager().install())
+    else:
+        print("Provide a valid browser name from this list chrome/firefox")
+    driver.maximize_window()
+    app_url = ReadConfigurations.read_configuration("basic info","url")
+    driver.get(app_url)
+    request.cls.driver = driver
+    yield
+    driver.quit()
---
+++ test/test_Login.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestLogin:
+    def test_login_with_valid_credentials(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("admin@luxelocker.com")
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("123456789aA!")
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Log In'])[1]").click()
+        # url = driver.current_url
+        print(self.driver.current_url)
+
+    def test_login_with_invalid_email_and_valid_password(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys(self.generate_email_with_time_stamp())
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("123456789aA!")
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Log In'])[1]").click()
+        expected_warning_message = "The email you entered did not match our records. Please double-check and try again."
+        self.driver.implicitly_wait(10)
+        assert self.driver.find_element(By.XPATH, "(//span[@class='text-xs'])[1]").text.__contains__(expected_warning_message)
+        # assert expected_warning_message == error_message
+
+    def test_login_with_valid_email_and_invalid_password(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("admin@luxelocker.com")
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("1234567899aA!")
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Log In'])[1]").click()
+        expected_warning_message = "The password you entered did not match our records. Please double-check and try again."
+        self.driver.implicitly_wait(10)
+        assert self.driver.find_element(By.XPATH, "//span[@class='text-xs']").text.__contains__(expected_warning_message)
+
+    def test_login_without_entering_credentials(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("")
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("")
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Log In'])[1]").click()
+        self.driver.implicitly_wait(10)
+        expected_warning_message = "Email is required"
+        assert self.driver.find_element(By.XPATH, "//span[@class='text-xs']").text.__contains__(expected_warning_message)
+
+
+
+    def generate_email_with_time_stamp(self):
+            time_stamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+            return "wajdanali" + time_stamp + "@gmail.com"
