df73bd8c5dc86335eb25ed0d4e5948de9f9252e6
Usama <wajdanali933@gmail.com>
2023-05-31 11:41:12 +0500

    Filter functionality in the Campuses page

---
+++ test/test_Filter.py
@@ -0,0 +1,24 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestFilter:
+    def test_filter_is_working_for_campus(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("admin@luxelocker.com")
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("123456789aA!")
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Log In'])[1]").click()
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/ul/div[1]/div[1]/div/div[2]/span").click()
+        self.driver.implicitly_wait(15)
+        ele_dis = self.driver.find_element(By.XPATH, "//p[normalize-space()='Filter']")
+        if ele_dis.is_enabled():
+                ele_dis.click()
+                print("This will remain pending till the Filter option will work in the campuses page")
+        else:
+             print("Filter button is not enabled in Campus section")
+
---
+++ test/test_Rowsperpage.py
@@ -0,0 +1,26 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestRowsPerPage:
+    def test_verifying_rows_per_page_with_total_rows(self):
+        self.driver.find_element(By.XPATH, "//input[@name='email']").send_keys("admin@luxelocker.com")
+        self.driver.find_element(By.XPATH, "//input[@name='password']").send_keys("123456789aA!")
+        self.driver.find_element(By.XPATH, "(//button[normalize-space()='Log In'])[1]").click()
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/ul/div[1]/div[1]/div/div[2]/span").click()
+        self.driver.implicitly_wait(15)
+        if self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div"):
+           dropdown_count = self.driver.find_elements(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div").text
+           totalfacilities = len(self.driver.find_elements(By.XPATH, "//*[@id=\"simple-tabpanel-0\"]/div/div[2]/div/div[1]/div/table/tbody/tr"))
+           if dropdown_count == totalfacilities:
+                print ('Rows are 20')
+           else:
+                print("Need to click on  50 rows per page.")
+        else:
+             quit()
