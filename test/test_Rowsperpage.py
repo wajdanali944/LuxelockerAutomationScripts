028b9934e017fc47ce94dd57af57f7e1dc510e09
Usama <wajdanali933@gmail.com>
2023-06-26 16:27:39 +0500

    Automated rowsper page of the announcements page

--- test/test_Rowsperpage.py
+++ test/test_Rowsperpage.py
@@ -71,6 +71,48 @@ class TestRowsPerPage:
                 if not self.check_for_more_records_for_users_page(total=100, all_records=splitval[2]):
                     print("Rows are more than 100")
 
+    def test_verifying_rows_per_page_with_total_rows_for_faqs_page(self):
+        home_page = HomePage(self.driver)
+        home_page.enter_email_address("admin@luxelocker.com")
+        home_page.enter_password("123456789aA!")
+        home_page.click_on_login_button()
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/ul/div[1]/div[4]/div").click()  #click on FAQS on side window
+        self.driver.implicitly_wait(15)
+        self.driver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div")
+
+        totalpaginationcount = self.driver.find_element(By.XPATH,
+                                                            "//*[@id=\"simple-tabpanel-0\"]/div/div[2]/div/div[2]/div[1]/div[2]/span")
+        text = totalpaginationcount.text
+        splitval = text.split()
+
+        if not self.check_for_more_records(count=20, total_records=splitval[2]):
+            if not self.check_for_more_records(count=50, total_records=splitval[2]):
+                if not self.check_for_more_records(count=100, total_records=splitval[2]):
+                    print("Rows are more than 100")
+
+    def test_verifying_rows_per_page_with_total_rows_for_announcement_page(self):
+        home_page = HomePage(self.driver)
+        home_page.enter_email_address("admin@luxelocker.com")
+        home_page.enter_password("123456789aA!")
+        home_page.click_on_login_button()
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.XPATH,
+                                 "/html/body/div[1]/div/div[1]/ul/div[1]/div[5]/div/div[2]/span").click()  # click on Announcements on side window
+        self.driver.implicitly_wait(15)
+        self.driver.find_element(By.XPATH,
+                                 "/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[1]/div[1]/div") #rows per page xpath
+
+        totalpaginationcount = self.driver.find_element(By.XPATH,
+                                                        "//span[@class='Pagination-rowsLabel']")  #pagination xpath
+        text = totalpaginationcount.text
+        splitval = text.split()
+
+        if not self.check_for_more_records_for_announcement_page(ann_total=20, ann_all_records=splitval[2]):
+            if not self.check_for_more_records_for_announcement_page(ann_total=50, ann_all_records=splitval[2]):
+                if not self.check_for_more_records_for_announcement_page(ann_total=100, ann_all_records=splitval[2]):
+                    print("Rows are more than 100")
+
     def check_for_more_records(self, count, total_records):
         self.driver.find_element(By.XPATH,
                                  "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div").click()
@@ -87,14 +129,28 @@ class TestRowsPerPage:
 
     def check_for_more_records_for_users_page(self, total, all_records):
         self.driver.find_element(By.XPATH,
-                                 "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div[1]/div[1]/div/div").click()
+                                 "/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[1]/div[1]/div/div").click()
         totalfacilities = self.driver.find_elements(By.XPATH, f"//li[normalize-space()='{total}']")
         totalfacilities[0].click()
         rows_count = len(self.driver.find_elements(By.XPATH,
-                                                   "//html/body/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/table/tbody/tr"))
+                                                   "/html/body/div[1]/div/div[2]/div/div[3]/div/div[1]/div/table/tbody/tr[1]"))
 
         if rows_count >= int(all_records):
             print(f'Rows are under {total}')
+            return True
+        else:
+            return False
+
+    def check_for_more_records_for_announcement_page(self, ann_total, ann_all_records):
+        self.driver.find_element(By.XPATH,
+                                 "/html/body/div[1]/div/div[2]/div/div[3]/div/div[2]/div[1]/div[1]/div").click()
+        totalfacilities = self.driver.find_elements(By.XPATH, f"//li[normalize-space()='{ann_total}']")
+        totalfacilities[0].click()
+        rows_count = len(self.driver.find_elements(By.XPATH,
+                                                   "/html/body/div[1]/div/div[2]/div/div[3]/div/div[1]/div/table/tbody/tr"))
+
+        if rows_count >= int(ann_all_records):
+            print(f'Rows are under {ann_total}')
             return True
         else:
             return False
